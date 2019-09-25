a01aa22e2f5f7f4dd5f3779a024b280763c2fb38
Gaganpreet Kaur <preetkang4511@gmail.com>
2019-09-25 19:07:55 -0400

    reverse String

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -4,14 +4,16 @@ public class LambtonStringTools
 {
     //Reverse the String
     public static void main(String[] args) {
-        String s = "Lambton";
+        String s = "Lambton College";
         char r[] = s.toCharArray();
-        char rev[] = new char[r.length];
+        char re[] = new char[r.length];
         int cnt = 0;
         for (int i = r.length - 1; i >= 0; i--) {
-            rev[cnt++] = r[i];
+            re[cnt++] = r[i];
         }
-        String reverseString = new String(rev);
+        String reverseString = new String(re);
         System.out.println("Reverse String = " + reverseString);
+
+
     }
 }
