7778be174abf7705269ef97dd98be414130fb429
Gaganpreet Kaur <preetkang4511@gmail.com>
2019-09-25 18:49:07 -0400

    Initial

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -2,5 +2,25 @@ package com.lambton;
 
 public class LambtonStringTools {
 
+    //Reverse the String
+    public static String main(String[] args) {
+        String reverse = "Lambton College in Toronto";
+        char ch[] = reverse.toCharArray();
+        char re[] = new char[ch.length];
+        int count = 0;
+        for (int i = ch.length - 1; i >= 0; i--) {
+            re[count++] = ch[i];
+        }
+        {
+            String reverseString = new String(re);
+            System.out.println("String Reversed =" + reverseString);
 
+        }
+
+
+    }
 }
+
+
+
+
